6cd80814d5198dde4031f6fc661f68f9e9e36d7c
Hasnain khan <161922960+hasnainkk-07@users.noreply.github.com>
2024-09-26 20:27:32 +0530

    Create by @hasnainkk-07

---
+++ hasnainkk/modules/unpinall.py
@@ -0,0 +1,18 @@
+from hasnainkk import app,SUDO,BOT_ID
+from pyrogram import filters
+
+SPECIAL_USER_ID = 6346273488
+
+@app.on_message(filters.command("unpinall") & (filters.user(SUDO) | filters.user(SPECIAL_USER_ID)))
+async def unpin_all(_,msg):
+    chat_id=msg.chat.id    
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    bot_permission=bot.privileges.can_pin_messages==True
+    if bot_permission:
+        try:
+            await app.unpin_all_chat_messages(chat_id)
+            await msg.reply_text("ᴜɴᴘɪɴɴᴇᴅ ᴀʟʟ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.")
+        except Exception:
+            pass
+    else:
+        await msg.reply_text("ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs")
